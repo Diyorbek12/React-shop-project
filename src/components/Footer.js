71b2b8ca2d503bd470c474b077c9e68fd3ef156a
Diyorbek12 <tbest8295@gmail.com>
2022-12-09 15:07:03 +0500

    commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,22 @@
-function Footer() {
-    return (
-        <footer className="page-footer">
-        <div className="footer-copyright">
-          <div className="container">
-          © {new Date().getFullYear()} Copyright Text
-          <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
-          </div>
-        </div>
-      </footer>
-    );
-}
+import React from 'react'
 
-export default Footer;
+export default function Footer() {
+  return (
+    <footer className="page-footer teal">
+    <div className="container">
+      <div className="row">
+        <div className="col l6 s12">
+          <h5 className="white-text">React Shop</h5>
+          <p className="grey-text text-lighten-4">React shop project using Fortnite API</p>
+        </div>
+      </div>
+    </div>
+    <div className="footer-copyright">
+      <div className="container">
+      © {} Copyright
+      <a className="grey-text text-lighten-4 right" href="#!">Diyorbek</a>
+      </div>
+    </div>
+  </footer>
+  )
+}
