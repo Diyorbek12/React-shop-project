6941fc6e17915b3ef4deab550c6f1ba2eb3e98b3
Diyorbek12 <tbest8295@gmail.com>
2022-12-11 17:21:01 +0500

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ export default function Header() {
   return (
     <nav className='blue'>
     <div className="nav-wrapper">
-      <a href="#" className="brand-logo">React Shop</a>
+      <a href="#!" className="brand-logo">React Shop</a>
       <ul id="nav-mobile" className="right">
-        <li><a className="waves-effect waves-light orange btn hide-on-med-and-down">Sign In</a></li>
+        <li><a href="#!" className="waves-effect waves-light orange btn hide-on-med-and-down">Sign In</a></li>
       </ul>
     </div>
   </nav>
